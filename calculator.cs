d1e59e71445d30390b3455ed573c3382b7a365bb
Aniqbm <Aniq_bm@amikom.ac.id>
2023-03-13 17:03:11 +0700

    koreksi part 3

--- calculator.cs
+++ calculator.cs
@@ -4,13 +4,12 @@ class Calculator {
 	{
 		int a = 10;
 		int b = 6;
-	
-	
-		 Console.WriteLine("Hasil penambahan {0} + {1} = {2}", a,b, penambahan (a, b));
-         Console.WriteLine("Hasil pengurangan {0} - {1} = {2}", a,b, pengurangan (a, b));
-       
-         Console.WriteLine("\nTekan sembarang key untuk keluar");
-         Console.ReadKey();
+		Console.WriteLine("Hasil penambahan {0} + {1} = {2}", a,b, penambahan (a, b));
+		Console.WriteLine("Hasil pengurangan {0} - {1} = {2}", a,b, pengurangan (a, b));
+		
+		Console.WriteLine("\nTekan sembarang key untuk keluar");
+		Console.ReadKey();
+
 	}
 	static int penambahan(int a, int b)
 	{
